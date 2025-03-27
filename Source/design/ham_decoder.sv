076c4236c2115b6bc8e4d51fec446895f156d418
Aaron Salas <aasalas@estudiantec.cr>
2025-03-27 14:36:32 -0600

    MUXES para corregir palabra

--- Source/design/ham_decoder.sv
+++ Source/design/ham_decoder.sv
@@ -17,11 +17,13 @@ module ham_decoder(
         // Señal interna para la palabra corregida
     logic [6:0] corrected_e;
 
-    always_comb begin
-        corrected_e = e; // Inicialmente igual a e
-        if (p != 4'b0000)  // Si el síndrome es diferente de 0, hay error
-            corrected_e[p[2:0] - 1] = ~corrected_e[p[2:0] - 1]; // Invertir bit erróneo
-    end
+    assign corrected_e[0] = (p[2:0] == 3'b001) ? ~e[0] : e[0];
+    assign corrected_e[1] = (p[2:0] == 3'b010) ? ~e[1] : e[1];
+    assign corrected_e[2] = (p[2:0] == 3'b011) ? ~e[2] : e[2];
+    assign corrected_e[3] = (p[2:0] == 3'b100) ? ~e[3] : e[3];
+    assign corrected_e[4] = (p[2:0] == 3'b101) ? ~e[4] : e[4];
+    assign corrected_e[5] = (p[2:0] == 3'b110) ? ~e[5] : e[5];
+    assign corrected_e[6] = (p[2:0] == 3'b111) ? ~e[6] : e[6];
 
     // Extraer los bits de datos correctos
     assign c = {corrected_e[6], corrected_e[5], corrected_e[4], corrected_e[2]};
